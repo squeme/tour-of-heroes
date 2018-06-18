b05270a6f3c02610c47e3d61bc6ab9d9312d9f9e
Sergio Queme <sergioqueme@gmail.com>
2018-06-18 15:54:33 -0600

    finalizada la fase 4

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -8,22 +8,18 @@ import { HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-    hero = {
-    id: 1,
-    name: 'Windstorm'
-    };
-
+ 
     heroes = HEROES;
-
+   
     selectedHero: Hero;
-
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
-
-  constructor() { }
-
-  ngOnInit() {
+   
+   
+    constructor() { }
+   
+    ngOnInit() {
+    }
+   
+    onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+    }
   }
-
-}
